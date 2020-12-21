a1458a6b60dcaeffd25c520b2a6b4cf6970f80e4
Robson Oliveira da Costa <robsondeveloper@gmail.com>
2020-12-21 17:23:52 -0400

    login and logout

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Keycloak from "keycloak-js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      let keycloak = new Keycloak({
+        url: "http://localhost:8080/auth",
+        realm: "myrealm",
+        clientId: "app-vue"
+      });
+      keycloak.init({ pkceMethod: "S256" });
+      this.$store.dispatch("storeKeycloak", keycloak);
+    }
+  }
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <br />
+    <button
+      type="button"
+      @click="login"
+      class="btn btn-primary me-3"
+      v-if="!isAuthenticated"
+    >
+      Login
+    </button>
+    <button
+      type="button"
+      @click="logout"
+      class="btn btn-secondary"
+      v-if="isAuthenticated"
+    >
+      Logout
+    </button>
+
+    <h3 v-if="isAuthenticated">Usuário: {{ name }}</h3>
+
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +28,23 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  computed: {
+    ...mapGetters(["isAuthenticated", "name"]),
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login");
+    },
+    logout() {
+      this.$store.dispatch("logout");
+    },
+  },
 };
 </script>
